3494407d62cc0962d1ce3f003a54bee58c855f37
liuguangfu <is_liuguangfu@163.com>
2019-12-03 21:07:31 +0800

    流程梳理

--- debug/webpack.config.js
+++ debug/webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
 	// 基础目录，绝对路径，用于从配置中解析入口起点和loader，默认使用当前目录，也就是默认在哪里执行node命令的位置
 	context: __dirname,
 
+	target: 'web', // 默认是web
+
 	mode: 'development',
 
 	// 监听文件变动，实时进行打包
@@ -40,12 +42,18 @@ module.exports = {
 		// plugin的第一种写法，直接是个函数，接收的参数是compiler实例
 		{
 			apply: (compiler) => {
+				compiler.hooks.run.tap('runCallback', () => {
+					consola.info(' 我勾住了run方法')
+				})
 				consola.info(' 这里是个自定义插件A～')
 			}
 		},
 
 		// plugin的第二种写法，是个对象，对象拥有apply方法，接收的参数是compiler实例
 		(compiler) => {
+			compiler.hooks.afterEmit.tap('runCallback', () => {
+				consola.info(' 我勾住了afterEmit方法')
+			})
 			consola.info(' 这里是个自定义插件B～')
 		}
 	],
