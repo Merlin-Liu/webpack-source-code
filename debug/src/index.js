ab03466370a12a25c30cdc0a35bbd241574805ae
liuguangfu <is_liuguangfu@163.com>
2019-12-09 16:03:51 +0800

    流程梳理

--- debug/src/index.js
+++ debug/src/index.js
@@ -1,4 +1,4 @@
 const  consola = require('consola')
 import is from 'object.is'  // 这里引入一个小而美的第三方库，以此观察webpack如何处理第三方包
-consola.success('很高兴认识你，webpack132311')
+consola.success('我是一个大多21212121数的')
 consola.info(is(1,1))
