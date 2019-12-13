327d56bcf04e4e4c9e5926c2d3be928e3efba8a8
liuguangfu <is_liuguangfu@163.com>
2019-12-13 15:24:55 +0800

    NormalModule的dependencies收集入口理清

--- debug/src/index.js
+++ debug/src/index.js
@@ -1,10 +1,10 @@
 import consola from 'consola'
 import is from 'object.is'  // 这里引入一个小而美的第三方库，以此观察webpack如何处理第三方包
-import throttle from 'lodash.throttle'
+// import throttle from 'lodash.throttle'
 
-const fn = throttle(() => {
-	console.error('11212212')
-}, 3000)
+// const fn = throttle(() => {
+// 	console.error('11212212')
+// }, 3000)
 
 consola.success('i am webpack~~～～～')
 consola.info(is(1,1))
